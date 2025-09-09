e060f2f07432dfe2a0860e78cb5d0c533f5fde39
Oscar Ríos <oarg2704@gmail.com>
2025-09-08 20:12:59 -0500

    Modificación 2

--- PARCIALES/PARCIAL_1/gestionar_membresias.php
+++ PARCIALES/PARCIAL_1/gestionar_membresias.php
@@ -1,5 +1,5 @@
 <?php
-    include('funciones_gimnasio.php');
+    include 'funciones_gimnasio.php';
     $membresias= ['basica'=> 80,
     'premium'=> 120,
     'vip'=> 180, 
@@ -21,15 +21,17 @@
 </head>
 <body>
     <table>
-        <tr><th>Miembros del Gimnasio</th></tr>
-        <tr><th>Nombre</th><th>Precio</th><th>Cantidad</th></tr>
+        <tr><th>Miembros</th><th>Membresía</th><th>Antigüedad</th><th>Cuota base</th><th>Descuento Aplicado</th><th>Seguro médico</th><th>Cuota final</th></tr>
         <?php
             foreach ($miembros as $miembro=>$membresia){
+            $cuotaBase= $membresias["{$membresia['tipo']}"];
             $descuentoMonto=(calcular_promocion($membresia['antiguedad'])*100)."% - ". calcular_promocion($membresia['antiguedad'])* $membresias["{$membresia['tipo']}"] ;
+            $seguro=seguro_medico($cuotaBase);
+            $cuotaFinal=calcular_cuota_final($cuotaBase, calcular_promocion($membresia['antiguedad']),$seguro);
         ?>
         <tr><td><?=$miembro?></td><td><?=$membresia['tipo']?></td><td><?=$membresia['antiguedad']?></td>
         <td><?=$membresias["{$membresia['tipo']}"]?></td>
-        <td><?=$descuentoMonto ?></td> <td> </td></tr>
+        <td><?=$descuentoMonto ?></td> <td><?=$seguro?></td> <td><?=$cuotaFinal?></td></tr>
         <?php }?>
     </table>
 </body>
